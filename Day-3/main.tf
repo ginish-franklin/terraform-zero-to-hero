b9a3cfc314f236556cd2a50ea93ae2cf442a1d26
ginish-franklin <ginish.franklin@gmail.com>
2025-08-07 11:17:23 +0530

    Update main.tf
    added output block

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -8,3 +8,8 @@ module "ec2_instance" {
     instance_type_value = "t2.micro"
     subnet_id_value = "subnet-053c34acfaeb0e55e"
 }
+
+
+output "ec2_public_ip" {
+  value = module.ec2-instance.public-ip-address
+}
