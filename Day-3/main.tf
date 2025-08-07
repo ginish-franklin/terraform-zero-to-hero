11d7f70b380dec414a2ab44d781ae880a8fbf7de
ginish-franklin <ginish.franklin@gmail.com>
2025-08-07 11:30:45 +0530

    Update main.tf
    added ec2 name tag

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -7,6 +7,7 @@ module "ec2_instance" {
     ami_value = "ami-020cba7c55df1f615"
     instance_type_value = "t2.micro"
     subnet_id_value = "subnet-053c34acfaeb0e55e"
+    ec2_name = "Terraform-EC2-Day3"
 }
 
 
