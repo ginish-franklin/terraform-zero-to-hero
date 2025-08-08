8e5de7884559dd60b4afb868c7be1655f6a4711f
ginish-franklin <ginish.franklin@gmail.com>
2025-08-09 00:32:22 +0530

    Update main.tf

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,30 +3,29 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "http://3.85.5.119:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "81fef3ae-5abe-201b-2499-06b8361a4d6e"
+      secret_id = "eb209c21-54bc-e74d-f832-5a1b90241404"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
-  name  = "test-secret" // change it according to your secret
+  mount = "kv"
+  name  = "test-secret"
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-053b0d53c279acc90"
+resource "aws_instance" "vault-demo" {
+  ami = "ami-020cba7c55df1f615"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
